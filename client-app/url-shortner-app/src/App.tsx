f5af2c93fbba3b686c828d22d31ab62ba4a9c2af
P-KAVYASRI <kavyasri369369@gmail.com>
2026-05-08 16:22:31 +0530

    commit

--- client-app/url-shortner-app/src/App.tsx
+++ client-app/url-shortner-app/src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css'
+import Header from './compoents/Header/Header';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-center">
-  Hello world
-</h1>
-  )
+    <>
+      <Header />
+    </>
+  );
 }
 
-export default App
+export default App;
--- client-app/url-shortner-app/src/compoents/Header/Header.tsx
+++ client-app/url-shortner-app/src/compoents/Header/Header.tsx
@@ -1,14 +1,19 @@
 import * as React from 'react';
 
-export interface IHeaderProps {
-}
+export interface IHeaderProps {}
 
-export default class Header extends React.Component<IHeaderProps> {
-  public render() {
-    return (
-      <div>
-        
+const Header: React.FunctionComponent<IHeaderProps> = () => {
+  return (
+    <div className="bg-slate-900">
+      <div className="container mx-auto p-2">
+        <nav className="py-5">
+          <div className="text-base text-white font-semibold">
+            URLShortner
+          </div>
+        </nav>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Header;

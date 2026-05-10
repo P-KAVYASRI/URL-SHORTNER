c23a9ef238731ed455aeaf0d26fb5d668a28a521
P-KAVYASRI <kavyasri369369@gmail.com>
2026-05-10 16:14:01 +0530

    commit

--- client-app/url-shortner-app/src/components/Container/Container.tsx
+++ client-app/url-shortner-app/src/components/Container/Container.tsx
@@ -1,10 +1,29 @@
 import * as React from 'react';
 import FormContainer from '../FormContainer/FormContainer';
+import type { UrlData } from '../../interface/UrlData';
+import axios from 'axios';
+import { serverUrl } from '../../helpers/Constants';
 
 interface IContainerProps {
 }
 
 const Container: React.FunctionComponent<IContainerProps> = () => {
+  const [data, setData] = React.useState<UrlData[]>([]);
+  const fetchTableData = async () => {
+    try {
+      const response = await axios.get(`${serverUrl}/shortUrl`);
+      console.log("the response from server is :", response);
+      setData(response.data);
+      console.log("Data : ", response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  React.useEffect(() => {
+    fetchTableData();
+  }, []);
+
   return (<>
   <FormContainer />
   </>
